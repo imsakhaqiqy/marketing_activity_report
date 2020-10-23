f261c5cfa8264024aa18207f53d5958373654692
mathias andwiki <mathiasandwiki15@gmail.com>
2020-10-23 16:28:03 +0700

    Login Jadi Masuk

--- lib/Screens/Welcome/components/body.dart
+++ lib/Screens/Welcome/components/body.dart
@@ -32,7 +32,7 @@ class Body extends StatelessWidget {
             ),
             SizedBox(height: size.height * 0.05),
             RoundedButton(
-              text: "LOGIN",
+              text: "Masuk",
               color: green200,
               press: () {
                 Navigator.push(
